12afba2e6bf1610d62961e8d216305660ba087ac
mithulcrafts <codinggraphicsdesiging@gmail.com>
2025-12-09 17:21:36 +0530

    After completing the movie display portion

--- MovieDisplay/MovieDisplay.js
+++ MovieDisplay/MovieDisplay.js
@@ -6,5 +6,18 @@ async function display(movieid) {
     let tmdbURL = `https://api.themoviedb.org/3/movie/${movieid}?api_key=${API_KEY}`;
     const res=await fetch(tmdbURL);
     const data=await res.json();  
-    document.querySelector("h2").innerText=data.title;
+    document.querySelector(".banner")
+      .style.setProperty("--banner-image",
+        `url(https://image.tmdb.org/t/p/original${data.backdrop_path})`
+      );
+    document.querySelector(".poster").style.backgroundImage=`url(https://image.tmdb.org/t/p/original${data.poster_path})`;
+    document.querySelector(".Tagline").innerText=data.tagline;
+    if(!data.tagline)
+    {
+        document.querySelector(".banner").style.setProperty("--genres-top","18.5vh");
+    }
+    document.querySelector(".genres").innerText=data.genres.map(g=>g.name).join(", ");
+    document.querySelector(".Title").innerText=data.title;
+    document.querySelector(".Description").innerText=data.overview;
+    document.title=data.title;
 }
